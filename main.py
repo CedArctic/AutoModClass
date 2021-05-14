d7d12d81f06df7d2c7d2001a6697e6aa9eab3d7e
CedArctic <kdoumani98@gmail.com>
2021-05-14 14:13:39 +0300

    Restructured dataset importing and organization in main.py

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ import seaborn as sn
 # Import model
 model = keras.models.load_model('transferLearning/VGG-16/trained_model/vgg-16-tl-ds1-13-5')
 
-# Import validation dataset
-#TODO: Switch from a validation dataset to a test dataset. This is for testing purposes only.
-val_img_dir = pathlib.Path('transferLearning/VGG-16/dataset/validation')
-val_img_count = len(list(val_img_dir.glob('*/*/*.png')))
+# Dataset directories
+train_img_dir = pathlib.Path('dataset/training')
+test_img_dir = pathlib.Path('dataset/test')
+
+# Count images
+train_img_count = len(list(train_img_dir.glob('*/*/*.png')))
+test_img_count = len(list(test_img_dir.glob('*/*/*.png')))
+
+# Dataset Parameters
 img_height = 224
 img_width = 224
 img_channels = 3
@@ -32,40 +37,66 @@ mod_idx = {}
 for index, mod in enumerate(mod_schemes):
     mod_idx[mod] = index
 
-# Allocate and populate labels array traversing the Root -> (SNRs) -> (Modulation Schemes) tree for the training and
-# validation datasets
+# Allocate and populate labels array traversing the Root -> (SNRs) -> (Modulation Schemes) tree for the datasets
 # Label arrays indices
-val_labels_idx = 0
+train_labels_idx = 0
+test_labels_idx = 0
 
 # Label arrays
-val_labels = np.zeros(val_img_count, dtype=int)
+train_labels = np.zeros(train_img_count, dtype=int)
+test_labels = np.zeros(test_img_count, dtype=int)
 for snr in snrs:
     for mod in mod_schemes:
         # Number of samples for a specific SNR and modulation scheme in each dataset
-        val_snr_mod_samples = len(list(val_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
+        train_snr_mod_samples = len(list(train_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
+        val_snr_mod_samples = len(list(test_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
         # Write into label arrays the appropriate number of modulation scheme indices according to the number of samples
-        val_labels[val_labels_idx:val_labels_idx + val_snr_mod_samples] = mod_idx[mod] * np.ones(val_snr_mod_samples, dtype=int)
+        train_labels[train_labels_idx:train_labels_idx + train_snr_mod_samples] = mod_idx[mod] * np.ones(train_snr_mod_samples, dtype=int)
+        test_labels[test_labels_idx:test_labels_idx + val_snr_mod_samples] = mod_idx[mod] * np.ones(val_snr_mod_samples, dtype=int)
         # Increment the label array indices
-        val_labels_idx += val_snr_mod_samples
+        train_labels_idx += train_snr_mod_samples
+        test_labels_idx += val_snr_mod_samples
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    val_img_dir,
-    labels=val_labels.tolist(),
+# Training dataset structure
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    train_img_dir,
+    labels=train_labels.tolist(),
     label_mode='int',
-    # validation_split=0.2,
-    # subset="training",
-    # seed=123,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
     image_size=(img_height, img_width),
     batch_size=batch_size,
     shuffle=False)
 
+# Validation dataset structure
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    train_img_dir,
+    labels=train_labels.tolist(),
+    label_mode='int',
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+    shuffle=False)
+
+# Test dataset structure
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    test_img_dir,
+    labels=test_labels.tolist(),
+    label_mode='int',
+    seed=123,
+    shuffle=True,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
 # Make predictions
 print("Inference started")
-predictions = model.predict(val_ds)
+predictions = model.predict(test_ds)
 
-# Plot confusion matrix
-cm = confusion_matrix(val_labels, predictions.argmax(axis=1))
-
+# Generate and plot confusion matrix
+cm = confusion_matrix(test_labels, predictions.argmax(axis=1))
 sn.heatmap(cm, annot=True, fmt='g')
 
 # Plot training and validation accuracy and losses
