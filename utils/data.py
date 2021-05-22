f3f6875288a3a82ea631fcf4bfdf2f2a56e38a46
CedArctic <kdoumani98@gmail.com>
2021-05-22 13:58:06 +0300

    Switched dataset strucutre to mod_scheme/x_dB

--- utils/data.py
+++ utils/data.py
@@ -14,37 +14,38 @@ def load_data(mod_schemes, snrs, img_height, img_width, batch_size):
     train_img_count = len(list(train_img_dir.glob('*/*/*.png')))
     test_img_count = len(list(test_img_dir.glob('*/*/*.png')))
 
-    # Dictionary with index for each label
-    mod_idx = {}
-    for index, mod in enumerate(mod_schemes):
-        mod_idx[mod] = index
-
-    # Allocate and populate labels array traversing the Root -> (SNRs) -> (Modulation Schemes) tree for the datasets
-    # Label arrays indices
-    train_labels_idx = 0
-    test_labels_idx = 0
-
-    # Label arrays
-    train_labels = np.zeros(train_img_count, dtype=int)
-    test_labels = np.zeros(test_img_count, dtype=int)
-    for snr in snrs:
-        for mod in mod_schemes:
-            # Number of samples for a specific SNR and modulation scheme in each dataset
-            train_snr_mod_samples = len(list(train_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
-            val_snr_mod_samples = len(list(test_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
-            # Write into label arrays the appropriate number of modulation scheme indices according to the number of samples
-            train_labels[train_labels_idx:train_labels_idx + train_snr_mod_samples] = mod_idx[mod] * np.ones(
-                train_snr_mod_samples, dtype=int)
-            test_labels[test_labels_idx:test_labels_idx + val_snr_mod_samples] = mod_idx[mod] * np.ones(
-                val_snr_mod_samples, dtype=int)
-            # Increment the label array indices
-            train_labels_idx += train_snr_mod_samples
-            test_labels_idx += val_snr_mod_samples
+    # === Code used for the x_db/mod_scheme/ dataset structure ===
+    # # Dictionary with index for each label
+    # mod_idx = {}
+    # for index, mod in enumerate(mod_schemes):
+    #     mod_idx[mod] = index
+    #
+    # # Allocate and populate labels array traversing the Root -> (SNRs) -> (Modulation Schemes) tree for the datasets
+    # # Label arrays indices
+    # train_labels_idx = 0
+    # test_labels_idx = 0
+    #
+    # # Label arrays
+    # train_labels = np.zeros(train_img_count, dtype=int)
+    # test_labels = np.zeros(test_img_count, dtype=int)
+    # for snr in snrs:
+    #     for mod in mod_schemes:
+    #         # Number of samples for a specific SNR and modulation scheme in each dataset
+    #         train_snr_mod_samples = len(list(train_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
+    #         val_snr_mod_samples = len(list(test_img_dir.glob('{}_db/{}/*.png'.format(snr, mod))))
+    #         # Write into label arrays the appropriate number of modulation scheme indices according to the number of samples
+    #         train_labels[train_labels_idx:train_labels_idx + train_snr_mod_samples] = mod_idx[mod] * np.ones(
+    #             train_snr_mod_samples, dtype=int)
+    #         test_labels[test_labels_idx:test_labels_idx + val_snr_mod_samples] = mod_idx[mod] * np.ones(
+    #             val_snr_mod_samples, dtype=int)
+    #         # Increment the label array indices
+    #         train_labels_idx += train_snr_mod_samples
+    #         test_labels_idx += val_snr_mod_samples
 
     # Training dataset structure
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         train_img_dir,
-        labels=train_labels.tolist(),
+        # labels=train_labels.tolist(),
         label_mode='int',
         validation_split=0.2,
         subset="training",
@@ -56,7 +57,7 @@ def load_data(mod_schemes, snrs, img_height, img_width, batch_size):
     # Validation dataset structure
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         train_img_dir,
-        labels=train_labels.tolist(),
+        # labels=train_labels.tolist(),
         label_mode='int',
         validation_split=0.2,
         subset="validation",
@@ -68,10 +69,13 @@ def load_data(mod_schemes, snrs, img_height, img_width, batch_size):
     # Test dataset structure
     test_ds = tf.keras.preprocessing.image_dataset_from_directory(
         test_img_dir,
-        labels=test_labels.tolist(),
+        # labels=test_labels.tolist(),
         label_mode='int',
         image_size=(img_height, img_width),
         batch_size=batch_size,
         shuffle=False)
+
+    # Get testset labels
+    test_labels = np.concatenate([y for x, y in test_ds], axis=0)
 
     return train_ds, val_ds, test_ds, test_labels
