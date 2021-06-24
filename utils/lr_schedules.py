0e4c9f163a662fa7516c274762d4571588689004
CedArctic <kdoumani98@gmail.com>
2021-06-24 19:17:16 +0300

    lr_schedules.py: Added Step Decay and Triangular Cyclic Schedulers

--- utils/lr_schedules.py
+++ utils/lr_schedules.py
@@ -1,4 +1,4 @@
-from math import cos, pi, exp
+from math import cos, pi, exp, floor
 
 warmup_rate = 0.1
 
@@ -13,10 +13,39 @@ def szegedy_schedule(epoch, lr):
 
 def cosine_annealing_schedule(epoch, lr):
     epochs_total = 30
-    lr = (1/2) * (1 + cos(epoch * pi / epochs_total)) * lr
+    lr = (1 / 2) * (1 + cos(epoch * pi / epochs_total)) * lr
     return lr
 
 
 def exp_decay_schedule(epoch, lr):
     initial_lr = 0.001
     return initial_lr * exp(-0.1 * epoch)
+
+
+# Also known as Learning Rate Annealing
+def step_decay_schedule(epoch, lr):
+    # Number of epochs after which the learning rate should drop
+    epochs_drop = 10
+    # Constant rate at which the drop happens
+    drop = 0.1
+    # Initial Learning Rate
+    initial_lr = 0.001
+
+    lr = initial_lr * (drop ** floor(epoch / epochs_drop))
+    return lr
+
+
+def triangular_cyclic_schedule(epoch, lr):
+    # Minimum and Maximum Learning Rates
+    lr_min = 0.00001
+    lr_max = 0.001
+
+    # Number of epochs for a cycle
+    stepsize = 10
+
+    cycle = floor(1 + epoch / (2 * stepsize))
+    x = abs(epoch / stepsize - 2 * cycle + 1)
+    lr = lr_min + (lr_max - lr_min) * max(0, 1 - x)
+
+    return lr
+
