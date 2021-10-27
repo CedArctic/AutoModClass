baddad6f2a7371073af21ea1f25987510f675f5d
kostino <triaridis@ece.auth.gr>
2021-10-27 19:25:19 +0300

    Utilize new hybrid data loading module.

--- train_hybrid.py
+++ train_hybrid.py
@@ -11,14 +11,15 @@ import seaborn as sn
 from sklearn.utils import shuffle
 
 from utils.plotting import plotAccLoss
-from utils.data import load_data, get_dataset, get_selected_dataset
+from utils.data import load_hybrid_data, get_dataset, get_selected_dataset, get_image_dataset
 from hybrid_model import hybrid_model
-
+from transferLearning.resnet import resnet
+from utils.lr_schedules import exp_decay_schedule, szegedy_schedule, cosine_annealing_schedule, step_decay_schedule, triangular_cyclic_schedule
 
 os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
 # === Load Data ===
-MODEL_NAME = 'HYBRID-1'
-cnn_name = 'VGG-frozen-Dropout-batch-100-GAP-DATASET-3-DYNAMIC'
+MODEL_NAME = 'HYBRID_MODEL_TEST1'
+cnn_name = 'RESNET-152v2-SGD-LRPLAT-LR1e-3-MOMENTUM-EXP_DECAY'
 fc_name = 'fc-2-512'
 print('Using CNN Model: {}'.format(cnn_name))
 print('Using FC Model: {}'.format(cnn_name))
@@ -56,16 +57,27 @@ snrs.sort()
 
 
 # Dataset caching and prefetching
+# AUTOTUNE = tf.data.AUTOTUNE
+# train_ds = get_image_dataset(['tfrecords/shard_{}.tfrecords'.format(i) for i in range(16)], ordered=False)
+# val_ds = get_image_dataset(['tfrecords/shard_{}.tfrecords'.format(i) for i in range(16,20)], ordered=False)
+#
+# train_ds = train_ds.shuffle(10000, reshuffle_each_iteration=True)
+# val_ds = val_ds.shuffle(10000, reshuffle_each_iteration=True)
+#
+#
+# train_ds = train_ds.batch(batch_size)
+# val_ds = val_ds.batch(batch_size)
+# # train_ds = train_ds.repeat()
+# train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+# val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+
+
+# Load data
+#TODO: Separate method for test datasets loading to improve performance
+train_ds, val_ds, test_ds, test_labels = load_hybrid_data(mod_schemes, snrs, img_height, img_width, batch_size)
+
+# Dataset caching and prefetching
 AUTOTUNE = tf.data.AUTOTUNE
-train_ds = get_dataset(['trainds/{}_{}.tfrecords'.format(mod, snr) for mod in mod_schemes for snr in snrs], ordered=False)
-train_ds = train_ds.shuffle(10000, reshuffle_each_iteration=True)
-val_ds = get_dataset(['valds/{}_{}.tfrecords'.format(mod, snr) for mod in mod_schemes for snr in snrs], ordered=False)
-val_ds = val_ds.shuffle(10000, reshuffle_each_iteration=True)
-
-
-train_ds = train_ds.batch(batch_size)
-val_ds = val_ds.batch(batch_size)
-# train_ds = train_ds.repeat()
 train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
 
@@ -76,16 +88,16 @@ epochs = 30
 
 # Create model
 model = hybrid_model(cnn_name, fc_name)
-
+# model = resnet(img_height, img_width, std_input=False)
 # Print model summary
 model.summary()
 keras.utils.plot_model(model, "hybrid_model.png", show_shapes=True)
 
 # Compile model
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9), loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
 
 # Add early stopping
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=8)
 if not os.path.isdir("checkpoints"):
     os.makedirs('checkpoints')
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -94,10 +106,11 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     monitor='val_loss',
     mode='min',
     save_best_only=True)
+lr_schedule = tf.keras.callbacks.LearningRateScheduler(szegedy_schedule)
 # Train model
 #TODO: Perhaps add ModelCheckpoint, Tensorboard, EarlyStopping and other CallBack functions
 history = model.fit(train_ds, batch_size=batch_size, epochs=epochs, validation_data=val_ds,
-                    callbacks=[early_stop, model_checkpoint_callback])
+                    callbacks=[early_stop, model_checkpoint_callback, lr_schedule])
 
 # Save Model
 if not os.path.isdir("trained_models"):
